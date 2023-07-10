dcc8111988a4500e7e747d22a360e2578dc10381
KaoutherBz <k.bouzouita@instadeep.com>
2023-07-10 11:48:31 +0100

    Test regression model and best hyper params

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import hydra
 from omegaconf import DictConfig
 
 _steps = [
-    "download",
-    "basic_cleaning",
-    "data_check",
-    "data_split",
-    "train_random_forest",
+    # "download",
+    # "basic_cleaning",
+    # "data_check",
+    # "data_split",
+    # "train_random_forest",
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
